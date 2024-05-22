7294a6097c0dedb931c8951e75405acbdde34cdf
Ssamk-kuu-ruu <142188212+Ssamk-kuu-ruu@users.noreply.github.com>
2024-05-22 20:54:59 +0800

    can now add person informations in the database

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from flask import Flask, make_response, jsonify
+from flask import Flask, make_response, jsonify, request
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
@@ -30,6 +30,25 @@ def get_persons():
     data = data_fetch(""" SELECT * FROM persons """)
 
     return make_response(jsonify(data), 200)
+
+@app.route("/persons", methods=["POST"])
+def add_persons():
+    cur = mysql.connection.cursor()
+    info = request.get_json()
+
+    name = info["Name"]
+    age = info["Age"]
+    email = info["Email"]
+
+    cur.execute(
+            """ INSERT INTO persons (Age, Email, Name) VALUES (%s, %s, %s)""", (age, email, name)
+    )
+    mysql.connection.commit()
+    print("row(s) affected {}".format(cur.rowcount))
+    rows_affected = cur.rowcount
+    cur.close()
+
+    return make_response(jsonify({"message":"person added successfully", "rowsaffected": rows_affected}), 201)
 
 @app.route("/persons/<int:id>", methods=["GET"])
 def get_persons_by_id(id):
